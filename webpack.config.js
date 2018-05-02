62cdc547040fa12634f6662153d087d9bb05ddb0
Andrey <andrey_9112@mail.ru>
2018-05-02 13:01:10 +0300

    ошибка

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,23 @@
+import WebpackCleanupPlugin from 'webpack-cleanup-plugin';
 const path = require('path');
 
 module.exports = {
   mode: 'none',
   entry: './source/js/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'js/bundle.js',
     path: path.join(__dirname, 'build/js')
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+
+  plugins: [
+    new WebpackCleanupPlugin()
+  ]
 };
