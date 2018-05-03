4b6cf3a1446d566d6e931914517e9d70e8443454
Andrey <andrey_9112@mail.ru>
2018-05-03 23:59:46 +0300

    Промежуточный коммит

--- webpack/file-loader.js
+++ webpack/file-loader.js
@@ -2,13 +2,13 @@ module.exports = function () {
   return {
     module: {
       rules: [
-        // {
-        //   test: /\.(jpg|png|svg)$/,
-        //   loader: 'file-loader',
-        //   options: {
-        //     name: 'img/[name].[ext]'
-        //   }
-        // },
+        {
+          test: /\.(jpg|png|svg)$/,
+          loader: 'file-loader',
+          options: {
+            name: 'img/[name].[ext]'
+          }
+        },
 
         {
           test: /\.(woff|woff2)$/,
@@ -16,39 +16,6 @@ module.exports = function () {
           options: {
             name: 'fonts/[name].[ext]'
           }
-        },
-
-        {
-          loaders: [{
-            test: /\.(gif|png|jpe?g|svg)$/i,
-            use: [
-              'file-loader',
-              {
-                loader: 'image-webpack-loader',
-                options: {
-                  mozjpeg: {
-                    progressive: true,
-                    quality: 65
-                  },
-
-                  optipng: {
-                    optimizationLevel: 3
-                  },
-                  pngquant: {
-                    quality: '65-90',
-                    speed: 4
-                  },
-                  gifsicle: {
-                    interlaced: false,
-                  },
-
-                  webp: {
-                    quality: 75
-                  }
-                }
-              },
-            ],
-          }]
         }
       ]
     }
